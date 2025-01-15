ed4df1d867e67af994744d7d0f5b21a9e03cfde1
Nicolas <nicojs2011@gmail.com>
2025-01-14 22:18:07 -0400

    blog

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,8 @@
-import { Router } from "express";
+import { request, Router } from "express";
 
 const router = Router();
 
+// ===== Views =====//
 router.get("/", (req, res) => {res.render('home', {title: 'JC Painting Contractors',page:"home"})})
 router.get("/blog", (req, res) => {res.render('blog', {title: 'JC Painting Contractors',page:"blog"})})
 router.get("/services", (req, res) => {res.render('services', {title: 'JC Painting Contractors',page:"services"})})
@@ -9,6 +10,8 @@ router.get("/contact", (req, res) => {res.render('contact', {title: 'JC Painting
 router.get("/gallery", (req, res) => {res.render('gallery', {title: 'JC Painting Contractors',page:"gallery"})})
 router.get("/about", (req, res) => {res.render('about', {title: 'JC Painting Contractors',page:"about"})})
 
+// 404
 router.get('*', (req, res) => {res.render('404', {title: 'Page no found'})})
+
 
 export default router
